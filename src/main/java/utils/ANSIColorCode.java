00bf756063281b9db6c73f51d07b396385f8ac0e
Kakarot-Abhi <mishraabhishek79333@gmail.com>
2022-11-03 23:34:08 +0530

    RelatedTo TemplateGenrator for solving problems fastly

---
+++ src/main/java/utils/ANSIColorCode.java
@@ -0,0 +1,23 @@
+package utils;
+
+public interface ANSIColorCode {
+
+    String BLACK = "\u001B[30m";
+    String BLACK_BACKGROUND = "\u001B[40m";
+    String RED = "\u001B[31m";
+    String RED_BACKGROUND = "\u001B[41m";
+    String GREEN = "\u001B[32m";
+    String GREEN_BACKGROUND = "\u001B[42m";
+    String YELLOW = "\u001B[33m";
+    String YELLOW_BACKGROUND = "\u001B[43m";
+    String BLUE = "\u001B[34m";
+    String BLUE_BACKGROUND = "\u001B[44m";
+    String PURPLE = "\u001B[35m";
+    String PURPLE_BACKGROUND = "\u001B[45m";
+    String CYAN = "\u001B[36m";
+    String CYAN_BACKGROUND = "\u001B[46m";
+    String WHITE = "\u001B[37m";
+    String WHITE_BACKGROUND = "\u001B[47m";
+    public static final String ANSI_RESET = "\u001B[0m";
+
+}
